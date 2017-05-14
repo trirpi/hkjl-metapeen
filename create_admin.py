e64bdaf939e82b12e8b5a7efb2208446c2688b94
Tristan Trouwen <tristan.trouwen@gmail.com>
2017-05-14 18:15:13 +0200

    easier creation of admin

--- create_admin.py
+++ create_admin.py
@@ -1,8 +1,21 @@
+import sys
+
 from peen.orm.models import User
 from peen import db
 
-admin_name = 'admin_oke'
-admin_pass = 'okeisgoed'
+admin_name = ''
+admin_pass = ''
+
+if sys.version.info >= (3, 0):
+    admin_name = input('Admin name: ')
+    admin_pass = input('Admin password: ')
+else:
+    admin_name = raw_input('Admin name: ')
+    admin_pass = raw_input('Admin password: ')
+
+if admin_name is None or admin_pass is None:
+    print('Please provide a username and password.')
+    sys.exit(0)
 
 admin = User(username=admin_name)
 admin.set_password(admin_pass)
