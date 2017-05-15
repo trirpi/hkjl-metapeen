ed4c7eda0e5a2d8c01cdc8a15f90bb50ce47c88e
Tristan Trouwen <tristan.trouwen@gmail.com>
2017-05-15 19:26:25 +0200

    changed update score functions

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -5,46 +5,51 @@ from crawler.ht_crawler import HTCrawler
 from crawler.otw_crawler import OTWCrawler
 from crawler.rm_crawler import RMCrawler
 from crawler.hts_crawler import HTSCrawler
+from crawler.nf_crawler import NFCrawler
 
 crawler_callbacks = {
     'cs': CSCrawler,
     'otw': OTWCrawler,
     'ht': HTCrawler,
     'rm': RMCrawler,
-    'hts': HTSCrawler
+    'hts': HTSCrawler,
+    'nf': NFCrawler,
 }
 
 
-def update_score(hacker, site=None):
+def update_score(hacker, site):
+    """
+    Update score of a user of a specific site
+    :param hacker: hacker class instance
+    :param site: site abbreviation like 'rm'
+    :return: 
+    """
+    try:
+        crawler = crawler_callbacks[site](hacker.username)  # create crawler instance for specific user
+        score = crawler.get_score()  # get score
+
+        hacker.update_score(site, score)  # set new values
+        db.session.commit()
+    except KeyError:  # if the site is not valid
+        print('[*] Error: The site ' + site + ' from user ' + hacker.username + ' does not exist.')
+
+
+def update_all_scores(hacker):
     """
     Update scores of a user.
     :param hacker: person whose score should be updated
     :type hacker: Hacker class instance
-    :param site: site that needs to be updated (if none it updates all scores)
     :return: None
 
     :Example:
 
     >>> from peen.orm.models import Hacker
     >>> hacker = Hacker.query.filter_by(username='henk').first()
-    >>> update_score(hacker, site='rm')
+    >>> update_all_scores(hacker)
     """
-    scores = hacker.scores  # list with username/id and current score
-    username = hacker.username
 
-    if site is None:  # update scores of all sites the user has
-        for site_key in scores:
-            crawler = crawler_callbacks[site_key](username)  # create crawler instance for specific user
-            score = crawler.get_score()  # get score
-
-            hacker.update_score(site_key, score)  # set new values
-    else:  # update the specific site
-        crawler = crawler_callbacks[site](username)  # create crawler instance for specific user with the correct site
-        score = crawler.get_score()  # get score
-
-        hacker.update_score(site, score)  # set new values
-
-    db.session.commit()  # and put them in the db
+    for site_key in hacker.scores:
+        update_score(hacker, site_key)
 
 
 def update_total_score(hacker):
