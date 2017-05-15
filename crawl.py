df90b9e91a54c6891ef1ee7626457e84ec96c045
TriRpi <Tplt0010!>
2017-05-15 19:26:25 +0200

    changed update score functions

--- crawl.py
+++ crawl.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
-from crawler.crawler import update_score, update_total_score
+from crawler.crawler import update_all_scores, update_total_score
 from peen.orm.models import Hacker
 
 hackers = Hacker.query.all()
 
 for hacker in hackers:
-    update_score(hacker)
+    update_all_scores(hacker)
     update_total_score(hacker)
