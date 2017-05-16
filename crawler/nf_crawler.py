12abc22d37def1558d054f48a5948249d29817a5
Tristan Trouwen <tristan.trouwen@gmail.com>
2017-05-16 18:59:54 +0200

    added error handling for nf

--- crawler/nf_crawler.py
+++ crawler/nf_crawler.py
@@ -14,5 +14,9 @@ class NFCrawler(BaseCrawler):
         """
         response = self.get_page_content(self.site)
 
-        score = re.findall(r'\d+', response.find_all("span")[0].text)[0]
+        try:
+            score = re.findall(r'\d+', response.find_all("span")[0].text)[0]
+        except IndexError:
+            print('[*] Info: User ' + self.username + ' does not have an account on nf.')
+            score = 0
         return int(score)
