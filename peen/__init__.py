5e7a7f557ee59954f6d715e3885877e8ae62fc04
Tristan Trouwen <tristan.trouwen@gmail.com>
2017-05-20 10:42:27 +0200

    improved api

--- peen/__init__.py
+++ peen/__init__.py
@@ -5,8 +5,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_restful import Api
-
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -17,8 +15,6 @@ db.create_all()
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
-
-api = Api(app)
 
 import peen.views
 import peen.admin.views
