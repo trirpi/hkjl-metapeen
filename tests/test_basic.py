56beab068bc137787b03f64d893fd77b05f3e0c5
trirpi <tristan.trouwen@gmail.com>
2017-06-12 17:28:45 +0200

    better testing

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,12 +1,22 @@
 import unittest
+from flask import current_app
+from peen import create_app, db
 
 
 class BasicTestCase(unittest.TestCase):
-    def setup(self):
-        pass
+    def setUp(self):
+        self.app = create_app('testing')
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
 
     def tearDown(self):
-        pass
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
 
-    def test_test(self):
-        print('hell')
+    def test_app_exists(self):
+        self.assertFalse(current_app is None)
+
+    def test_app_is_testing(self):
+        self.assertTrue(current_app.config['TESTING'])
